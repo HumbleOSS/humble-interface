6230bc8dca65868b5f54276668c82e16e3a7bbe7
Nicholas Shellabarger <temptemp3@users.noreply.github.com>
2025-04-03 14:17:36 -0600

    fetch pools from api only

--- src/store/poolSlice.ts
+++ src/store/poolSlice.ts
@@ -25,12 +25,11 @@ export interface PoolState {
 
 export const fetchPool = async (poolId: number) => {
   const { algodClient, indexerClient } = getAlgorandClients();
-  const InfoR = await new swap(poolId, algodClient, indexerClient)
-    .Info()
-  if(InfoR.success) {
+  const InfoR = await new swap(poolId, algodClient, indexerClient).Info();
+  if (InfoR.success) {
     return InfoR.returnValue;
   }
-}
+};
 
 export const getPool = async (poolId: number) => {
   try {
@@ -40,20 +39,20 @@ export const getPool = async (poolId: number) => {
       return pool;
     }
     const poolFetchResponse = await fetchPool(poolId);
-    if(poolFetchResponse) {
+    if (poolFetchResponse) {
       const newPool = {
         poolId,
         round: 0,
         tokA: poolFetchResponse.tokA,
         tokB: poolFetchResponse.tokB,
-      }
+      };
       await db.table("pools").put(newPool);
       return newPool;
     }
   } catch (error) {
     throw error;
   }
-}
+};
 
 export const getPools = createAsyncThunk<
   Pool[],
@@ -64,98 +63,100 @@ export const getPools = createAsyncThunk<
     const poolsTable = db.table("pools");
     const pools = await poolsTable.toArray();
     const lastRound = pools.reduce((acc, val) => Math.max(acc, val.round), 0);
-    if (pools.length === 0) {
-      const { data } = await axios.get(
-        `https://mainnet-idx.nautilus.sh/nft-indexer/v1/dex/pools`,
-        {
-          params: {
-            ["mint-min-round"]: lastRound,
-          },
-        }
-      );
-      const appPools = data.pools
-        .filter((p: any) => {
-          return p.providerId === "01";
-        })
-        .map((p: any) => ({
-          poolId: p.contractId,
-          tokA: Number(p.tokAId),
-          tokB: Number(p.tokBId),
-          round: p.mintRound,
-        }));
-      await db.table("pools").bulkPut(appPools);
-      return appPools;
-    }
-    const minRound = 0;
-    const { algodClient, indexerClient } = getAlgorandClients();
-    const ci = new CONTRACT(
-      CTCINFO_TRI,
-      algodClient,
-      indexerClient,
+    //if (pools.length === 0) {
+    const { data } = await axios.get(
+      `https://mainnet-idx.nautilus.sh/nft-indexer/v1/dex/pools`,
       {
-        name: "",
-        desc: "",
-        methods: [],
-        events: [
-          { name: "Register", args: [{ type: "(uint64,uint64,uint64)" }] },
-        ],
-      },
-      {
-        addr: "G3MSA75OZEJTCCENOJDLDJK7UD7E2K5DNC7FVHCNOV7E3I4DTXTOWDUIFQ",
-        sk: new Uint8Array(0),
+        params: {
+          //["mint-min-round"]: lastRound,
+        },
       }
     );
-    const makeCi = (ctcInfo: number) =>
-      new arc200(ctcInfo, algodClient, indexerClient, {
-        acc: {
-          addr: "G3MSA75OZEJTCCENOJDLDJK7UD7E2K5DNC7FVHCNOV7E3I4DTXTOWDUIFQ",
-          sk: new Uint8Array(0),
-        },
-        formatBytes: true,
-      });
+    console.log("data", data);
+    const appPools = data.pools
+      .filter((p: any) => {
+        return p.providerId === "01";
+      })
+      .map((p: any) => ({
+        poolId: p.contractId,
+        tokA: Number(p.tokAId),
+        tokB: Number(p.tokBId),
+        round: p.mintRound,
+      }));
+    await db.table("pools").bulkPut(appPools);
+    return appPools;
+    //}
 
-    const events = await ci.Register({
-      minRound: lastRound,
-    });
-    const newPools = events
-      .filter((event: any) => event[1] > lastRound)
-      .map((event: any) => {
-        return {
-          //txId: event[0],
-          round: event[1],
-          //ts: event[2],
-          poolId: Number(event[3][0]),
-          tokA: Number(event[3][1]),
-          tokB: Number(event[3][2]),
-        };
-      });
-    if (pools.length > 0) {
-      newPools.push(...pools);
-    }
-    for (const pool of newPools.filter(
-      (pool: PoolI) => pool?.round || 0 >= minRound
-    )) {
-      await db.table("pools").bulkPut(
-        newPools.map((pool: Pool) => {
-          return {
-            //txId: pool.txId,
-            round: pool.round,
-            //ts: pool.ts,
-            poolId: pool.poolId,
-            tokA: pool.tokA,
-            tokB: pool.tokB,
-          };
-        })
-      );
-    }
-    const wl: number[] = [];
-    const combinedPools = [...pools, ...newPools].filter(
-      (pool) =>
-        pool.round >= minRound &&
-        (wl.length > 0 || !wl.includes(pool.poolId)) &&
-        !BAD_POOLS.includes(pool.poolId)
-    );
-    return combinedPools;
+    // const minRound = 0;
+    // const { algodClient, indexerClient } = getAlgorandClients();
+    // const ci = new CONTRACT(
+    //   CTCINFO_TRI,
+    //   algodClient,
+    //   indexerClient,
+    //   {
+    //     name: "",
+    //     desc: "",
+    //     methods: [],
+    //     events: [
+    //       { name: "Register", args: [{ type: "(uint64,uint64,uint64)" }] },
+    //     ],
+    //   },
+    //   {
+    //     addr: "G3MSA75OZEJTCCENOJDLDJK7UD7E2K5DNC7FVHCNOV7E3I4DTXTOWDUIFQ",
+    //     sk: new Uint8Array(0),
+    //   }
+    // );
+    // const makeCi = (ctcInfo: number) =>
+    //   new arc200(ctcInfo, algodClient, indexerClient, {
+    //     acc: {
+    //       addr: "G3MSA75OZEJTCCENOJDLDJK7UD7E2K5DNC7FVHCNOV7E3I4DTXTOWDUIFQ",
+    //       sk: new Uint8Array(0),
+    //     },
+    //     formatBytes: true,
+    //   });
+
+    // const events = await ci.Register({
+    //   minRound: lastRound,
+    // });
+    // const newPools = events
+    //   .filter((event: any) => event[1] > lastRound)
+    //   .map((event: any) => {
+    //     return {
+    //       //txId: event[0],
+    //       round: event[1],
+    //       //ts: event[2],
+    //       poolId: Number(event[3][0]),
+    //       tokA: Number(event[3][1]),
+    //       tokB: Number(event[3][2]),
+    //     };
+    //   });
+    // if (pools.length > 0) {
+    //   newPools.push(...pools);
+    // }
+    // for (const pool of newPools.filter(
+    //   (pool: PoolI) => pool?.round || 0 >= minRound
+    // )) {
+    //   await db.table("pools").bulkPut(
+    //     newPools.map((pool: Pool) => {
+    //       return {
+    //         //txId: pool.txId,
+    //         round: pool.round,
+    //         //ts: pool.ts,
+    //         poolId: pool.poolId,
+    //         tokA: pool.tokA,
+    //         tokB: pool.tokB,
+    //       };
+    //     })
+    //   );
+    // }
+    // const wl: number[] = [];
+    // const combinedPools = [...pools, ...newPools].filter(
+    //   (pool) =>
+    //     pool.round >= minRound &&
+    //     (wl.length > 0 || !wl.includes(pool.poolId)) &&
+    //     !BAD_POOLS.includes(pool.poolId)
+    // );
+    // return combinedPools;
   } catch (error: any) {
     return rejectWithValue(error.message);
   }
