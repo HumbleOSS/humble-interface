6315791b81789bb3ed472f7055e3b711c3879181
Shelly <temptemp3@users.noreply.github.com>
2025-11-28 13:02:30 -0700

    refactor: switch from HashRouter to BrowserRouter
    Replace HashRouter with BrowserRouter to use standard browser history
    navigation instead of hash-based routing. This provides cleaner URLs
    without the hash fragment and better SEO support.
    
    Note: This change requires server configuration to handle client-side
    routing by redirecting all routes to index.html.
    
    Files Changed:
    - Modified: src/App.tsx (HashRouter -> BrowserRouter)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Provider, useSelector } from "react-redux";
 import store, { RootState } from "./store/store";
 import { routes } from "./routes";
